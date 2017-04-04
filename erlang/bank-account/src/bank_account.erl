80e013e74f79a37d3c7dbd0e37b877e36ce89ceb
joalon <joalon@localhost.localdomain>
2017-04-04 17:30:19 +0200

    Implemented create and balance

--- erlang/bank-account/src/bank_account.erl
+++ erlang/bank-account/src/bank_account.erl
@@ -1,13 +1,13 @@
 -module(bank_account).
 -behaviour(gen_server).
 
--export([create/0, deposit/2, withdraw/2]).
+-export([create/0, balance/1, deposit/2, withdraw/2]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
 
 %% API
 
 create() ->
-	gen_server:start_link(?MODULE, [], []).
+	gen_server:start_link(?MODULE, [0], []).
 
 
 deposit(BankAccount, Amount) ->
@@ -18,12 +18,15 @@ withdraw(BankAccount, Amount) ->
 	not_implemented_yet.
 
 
+balance(BankAccount) ->
+	gen_server:call(BankAccount, balance).
 
 
 %% Implementation
 
-handle_call(_,_,_) ->
-	not_implemented_yet.
+handle_call(balance, _From, Balance) ->
+	{ reply, Balance, Balance }.
+
 
 handle_cast(_,_) ->
 	not_implemented_yet.
@@ -31,8 +34,9 @@ handle_cast(_,_) ->
 handle_info(_,_) ->
 	not_implemented_yet.
 
-init(_) ->
-	not_implemented_yet.
+init(_Args) ->
+	{ok, 0}.
+
 
 terminate(_,_) ->
 	ok.
