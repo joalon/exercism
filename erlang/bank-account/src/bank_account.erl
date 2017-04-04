17c873000c8a97cd5b28010ad044791b476f5e47
joalon <joalon@localhost.localdomain>
2017-04-04 19:39:14 +0200

    Bugfixes

--- erlang/bank-account/src/bank_account.erl
+++ erlang/bank-account/src/bank_account.erl
@@ -1,31 +1,65 @@
 -module(bank_account).
 -behaviour(gen_server).
 
--export([create/0, balance/1, deposit/2, withdraw/2]).
+-export([create/0, balance/1, deposit/2, withdraw/2, charge/2, close/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
+-export([test_version/0]).
 
+test_version() ->
+	1.
 %% API
 
 create() ->
-	gen_server:start_link(?MODULE, [0], []).
+	{ok, Pid} = gen_server:start_link(?MODULE, [], []),
+	Pid.
 
 
 deposit(BankAccount, Amount) ->
-	not_implemented_yet.
+	{ok, Balance} = gen_server:call(BankAccount, {deposit, Amount}),
+	Balance.
 
 
 withdraw(BankAccount, Amount) -> 
-	not_implemented_yet.
+	{ok, WithdrawnAmount} = gen_server:call(BankAccount, {withdraw, Amount}),
+	WithdrawnAmount.
 
 
 balance(BankAccount) ->
-	gen_server:call(BankAccount, balance).
+	{ok, Balance} = gen_server:call(BankAccount, balance),
+	Balance.
 
+
+charge(BankAccount, Amount) ->
+	{ok, Balance} = gen_server:call(BankAccount, {charge, Amount}),
+	Amount.
+
+
+close(BankAccount) ->
+	ClosingBalance = balance(BankAccount),
+	gen_server:stop(BankAccount),
+	ClosingBalance.
 
 %% Implementation
 
-handle_call(balance, _From, Balance) ->
-	{ reply, Balance, Balance }.
+handle_call( balance, _From, Balance ) ->
+	{ reply, {ok, Balance}, Balance };
+
+handle_call( { withdraw, Amount }, _From, Balance ) when Amount =< Balance ->
+	NewBalance = Balance - Amount,
+	{ reply, {ok, NewBalance}, NewBalance };
+handle_call( { withdraw, Amount }, _From, Balance) ->
+
+	{ reply, { ok, Balance }, 0};
+
+handle_call( { deposit, Amount }, _From, Balance)  when Amount > 0 ->
+	NewBalance = Balance + Amount,
+	{ reply, { ok, NewBalance }, NewBalance };
+handle_call( { deposit, _Amount }, _From, Balance) ->
+	{ reply, { ok, 0 }, Balance};
+
+handle_call( { charge, Amount }, _From, Balance) ->
+	NewBalance = Balance - Amount,
+	{ reply, { ok, Amount }, NewBalance }.
 
 
 handle_cast(_,_) ->
