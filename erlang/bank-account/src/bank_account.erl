0fa1b7c80d7cab1e8ccdbc102bfea7c9ab4a41de
joalon <joalon@localhost.localdomain>
2017-04-04 17:12:17 +0200

    Started implementing bank account

--- erlang/bank-account/src/bank_account.erl
+++ erlang/bank-account/src/bank_account.erl
@@ -1,25 +1,27 @@
 -module(bank_account).
 -behaviour(gen_server).
 
+-export([create/0, deposit/2, withdraw/2]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
 
 %% API
 
 create() ->
+	gen_server:start_link(?MODULE, [], []).
+
+
+deposit(BankAccount, Amount) ->
 	not_implemented_yet.
 
 
-deposit() ->
-	not_implemented_yet.
-
-
-withdraw() -> 
+withdraw(BankAccount, Amount) -> 
 	not_implemented_yet.
 
 
 
 
 %% Implementation
+
 handle_call(_,_,_) ->
 	not_implemented_yet.
 
